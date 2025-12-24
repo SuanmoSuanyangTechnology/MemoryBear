e5f1aa18a9ed5ac9f9b9fed30591246f044976c6
yujiangping <yujiangping@taofen8.com>
2025-12-24 12:05:36 +0800

    fix:git commit

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Description: 
+ * @Version: 0.0.1
+ * @Author: yujiangping
+ * @Date: 2025-12-22 17:33:57
+ * @LastEditors: yujiangping
+ * @LastEditTime: 2025-12-24 12:01:18
+ */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
@@ -11,7 +19,8 @@ export default defineConfig({
     proxy: {
       // 主要API代理，支持 /api 和 /api/* 格式
       '/api': {
-        target: 'http://0.0.0.0:5173', // 后端服务地址
+        // target: 'http://0.0.0.0:5173', // 后端服务地址
+        target: 'https://devmemorybear.redbearai.com/',
         changeOrigin: true,
 
         // 匹配所有以/api开头的请求，包括/api/token
