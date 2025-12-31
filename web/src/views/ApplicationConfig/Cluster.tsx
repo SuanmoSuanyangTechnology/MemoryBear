0287e842e432a32b0e6a695854c15633614f0c74
zhaoying <yzhao96@best-inc.com>
2025-12-31 12:32:50 +0800

    feat(web): cluster handoffs disabled

--- web/src/views/ApplicationConfig/Cluster.tsx
+++ web/src/views/ApplicationConfig/Cluster.tsx
@@ -148,6 +148,7 @@ const Cluster = forwardRef<ClusterRef>((_props, ref) => {
                     value: type,
                     label: t(`application.${type}`),
                     labelDesc: t(`application.${type}Desc`),
+                    disabled: type === 'handoffs'
                   }))}
                   allowClear={false}
                 />
