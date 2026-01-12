f2ec58095ef5e0260dee8f86ab257c0466e89682
lixinyue <2569494688@qq.com>
2026-01-12 20:37:10 +0800

    情绪归类

--- api/app/services/memory_entity_relationship_service.py
+++ api/app/services/memory_entity_relationship_service.py
@@ -496,9 +496,15 @@ class MemoryEmotion:
             if emotion_type !=None:
                 length_data.append(emotion_intensity)
 
-            
+
             if emotion_type is not None and emotion_intensity is not None and formatted_created_at is not None:
                 # 使用(emotion_type, created_at)作为分组键
+                if emotion_type=='joy' or emotion_type=='surprise':
+                    emotion_type='positive'
+                elif emotion_type=='sadness' or emotion_type=='fear'  or emotion_type=='anger':
+                    emotion_type='negative'
+                elif emotion_type=='neutral':
+                    emotion_type='neutral'
                 group_key = (emotion_type, formatted_created_at)
                 
                 # 累加emotion_intensity
