fb76f765cc6995bf078b59c04d41a0de3c85a1f8
zhaoying <yzhao96@best-inc.com>
2026-02-03 14:01:28 +0800

    style(web): translate the comments in the web/src/api directory into English

--- web/src/api/member.ts
+++ web/src/api/member.ts
@@ -1,20 +1,26 @@
+/*
+ * @Author: ZhaoYing 
+ * @Date: 2026-02-03 14:00:01 
+ * @Last Modified by:   ZhaoYing 
+ * @Last Modified time: 2026-02-03 14:00:01 
+ */
 import { request } from '@/utils/request'
 
-// 成员列表
+// Member list
 export const memberListUrl = '/workspaces/members'
-// 邀请成员
+// Invite member
 export const inviteMember = (values: { email: string }) => {
   return request.post(`/workspaces/invites`, values)
 }
-// 删除成员
+// Delete member
 export const deleteMember = (id: string) => {
   return request.delete(`/workspaces/members/${id}`)
 }
-// 更新成员
+// Update member
 export const updateMember = (values: { id: string, role: string }) => {
   return request.put(`/workspaces/members`, [values])
 }
-// 验证邀请token
+// Validate invite token
 export const validateInviteToken = (token: string) => {
   return request.get(`/workspaces/invites/validate/${token}`)
 }
