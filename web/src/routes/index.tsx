9e6e8f50f8136fb8c963af34d9446dc49a237cad
zhaoying <yzhao96@best-inc.com>
2026-02-04 18:36:45 +0800

    feat(web): move prompt menu

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -3,7 +3,6 @@ import { createHashRouter, createRoutesFromElements, Route } from 'react-router-
 
 // 导入路由配置JSON
 import routesConfig from './routes.json';
-import Ontology from '@/views/Ontology';
 
 
 // 递归函数，用于生成路由元素
