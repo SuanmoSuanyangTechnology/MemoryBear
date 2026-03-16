8e6288bca8f038bfd5b44e713a5595afd9e63ee9
lanceyq <1982376970@qq.com>
2026-03-16 18:38:59 +0800

    [changes] Change the same reference

--- api/app/models/memory_perceptual_model.py
+++ api/app/models/memory_perceptual_model.py
@@ -7,8 +7,7 @@ from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.dialects.postgresql import JSONB
 
 from app.db import Base
-from app.schemas.app_schema import FileType
-
+from app.schemas import FileType
 
 class PerceptualType(IntEnum):
     VISION = 1
